6fa41aa53cc9a1913cf9d2c7404056ea4d0f6cc7
Emanuel Bodin <bodinemanuel@gmail.com>
2024-11-07 15:30:18 +0100

    added kustomize

--- app.js
+++ app.js
@@ -1,13 +1,18 @@
-const express = require("express");
+const express = require('express');
+require('dotenv').config();
 
 const app = express();
 
-app.get("/", (req, res) => {
-  res.send("Hello world");
+app.get('/', (req, res) => {
+  res.send(`Hello world, running in env: ${process.env.ENV}`);
 });
 
-app.get("/error", (req, res) => {
+app.get('/error', (req, res) => {
   process.exit();
 });
 
-app.listen(8080, () => console.log("listening on port 8080"));
+app.get('/healthz', (req, res) => {
+  res.send('OK');
+});
+
+app.listen(8080, () => console.log('listening on port 8080'));
